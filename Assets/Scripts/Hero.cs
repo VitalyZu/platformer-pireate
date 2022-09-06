4e7789880a76b1459cfd0fe1bdbd6bfb0f166fb8
VitalyZu <vitaly.z.vz@gmail.com>
2022-09-07 00:08:16 +0300

    add-rigidbody-move

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Hero : MonoBehaviour
 {
     [SerializeField] private float _speed;
+
+    private Rigidbody2D _rigidbody;
     private Vector2 _direction;
 
-    private void Update() 
+    private void Awake()
     {
-        if (_direction != Vector2.zero)
-        {
-            Vector3 delta = _direction * _speed * Time.deltaTime;
-            transform.position = transform.position + delta;
-        }
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(_direction.x, _rigidbody.velocity.y);
     }
 
     public void SetDirection(Vector2 direction)
