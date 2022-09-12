5e036177ae3af598f148d14f366c559e9c33bd49
VitalyZu <vitaly.z.vz@gmail.com>
2022-09-13 01:35:51 +0300

    add-player-simple-jump

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Hero : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private LayerMask _groundMask;
+
+    [SerializeField] private LayerCheck _layerCheck;
+
+    [SerializeField] private float _groundRadius;
+    [SerializeField] private Vector3 _groundDelta;
 
     private Rigidbody2D _rigidbody;
     private Vector2 _direction;
@@ -17,6 +24,13 @@ public class Hero : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+        bool isJumping = _direction.y > 0;
+
+        if (isJumping && IsGround())
+        {
+            _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+        }
     }
 
     public void SetDirection(Vector2 direction)
@@ -27,5 +41,18 @@ public class Hero : MonoBehaviour
     public void SaySomething()
     {
         Debug.Log("Say something");
+    }
+
+    public bool IsGround() 
+    {
+        //RaycastHit2D hit = Physics2D.CircleCast(transform.position + _groundDelta, _groundRadius, Vector2.down, 0, _groundMask);
+        //return hit.collider != null;
+        return _layerCheck.isTouchingLayer;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = IsGround() ? Color.green : Color.red;
+        Gizmos.DrawSphere(transform.position + _groundDelta, _groundRadius);
     }
 }
---
+++ Assets/Scripts/LayerCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerCheck : MonoBehaviour
+{
+    [SerializeField] private LayerMask _groundMask;
+    private Collider2D _collider;
+
+    public bool isTouchingLayer;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) 
+    {
+        isTouchingLayer = _collider.IsTouchingLayers(_groundMask);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isTouchingLayer = _collider.IsTouchingLayers(_groundMask);
+    }
+}
