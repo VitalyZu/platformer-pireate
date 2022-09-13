6b21a5826804ac760a95a6d7f1f676eeab67a497
VitalyZu <vitaly.z.vz@gmail.com>
2022-09-14 00:36:07 +0300

    modify-player-jump

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -10,9 +10,6 @@ public class Hero : MonoBehaviour
 
     [SerializeField] private LayerCheck _layerCheck;
 
-    [SerializeField] private float _groundRadius;
-    [SerializeField] private Vector3 _groundDelta;
-
     private Rigidbody2D _rigidbody;
     private Vector2 _direction;
 
@@ -27,9 +24,17 @@ public class Hero : MonoBehaviour
 
         bool isJumping = _direction.y > 0;
 
-        if (isJumping && IsGround())
+        if (isJumping)
         {
-            _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            if (IsGround())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+
+        }
+        else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * .5f);
         }
     }
 
@@ -45,14 +50,6 @@ public class Hero : MonoBehaviour
 
     public bool IsGround() 
     {
-        //RaycastHit2D hit = Physics2D.CircleCast(transform.position + _groundDelta, _groundRadius, Vector2.down, 0, _groundMask);
-        //return hit.collider != null;
         return _layerCheck.isTouchingLayer;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = IsGround() ? Color.green : Color.red;
-        Gizmos.DrawSphere(transform.position + _groundDelta, _groundRadius);
     }
 }
