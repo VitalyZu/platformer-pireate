3d0702531953101cecbfebf1a68a89ba806eeff2
VitalyZu <vitaly.z.vz@gmail.com>
2022-09-14 17:38:06 +0300

    add-level-restart
    + ground check collider fix

---
+++ Assets/Scripts/Components/EnterTriggerComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnterTriggerComponent : MonoBehaviour
+{
+    [SerializeField] private string _tag;
+    [SerializeField] private UnityEvent _action;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(_tag))
+        {
+            _action?.Invoke();
+        }
+    }
+}
