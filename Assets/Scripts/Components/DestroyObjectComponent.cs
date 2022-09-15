2568c41406dd0fc9d33e8997e6968f4bfc492184
VitalyZu <vitaly.z.vz@gmail.com>
2022-09-15 23:31:15 +0300

    add-outdoor-tilemap

---
+++ Assets/Scripts/Components/DestroyObjectComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectComponent : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+    public void DestroySelf()
+    {
+        if (_gameObject != null)
+        {
+            Destroy(_gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+}
