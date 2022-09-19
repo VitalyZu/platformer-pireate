9ca6be85cd5cb2706d09039b53110ebe5a7f9bbb
VitalyZu <vitaly.z.vz@gmail.com>
2022-09-20 00:19:46 +0300

    follow-target
    add follow target

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _dump;
+    private void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * _dump);
+        Debug.Log(Time.deltaTime * _dump);
+    }
+}
