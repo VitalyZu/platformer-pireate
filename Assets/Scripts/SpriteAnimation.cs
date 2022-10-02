c12e9f1c1b1c92cff22609416d09828e8e217e71
VitalyZu <vitaly.z.vz@gmail.com>
2022-10-02 22:53:48 +0300

    add-spriteanimation-script
    Custom sprite animation script

---
+++ Assets/Scripts/SpriteAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAnimation : MonoBehaviour
+{
+    [SerializeField] private int _frameRate;
+    [SerializeField] private bool _loop;
+    [SerializeField] private Sprite[] _sprites;
+    [SerializeField] private UnityEvent _onComplete;
+
+    private SpriteRenderer _spriteRenderer;
+    private float _secondsPerFrame;
+    private int _currentSpriteIndex;
+    private float _nextFrameTime;
+
+    private bool _isPlaying = true;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _secondsPerFrame = 1f / _frameRate;
+        _nextFrameTime = Time.time + _secondsPerFrame;
+    }
+
+    private void Update()
+    {
+        if (_isPlaying && _nextFrameTime > Time.time) return;
+
+        if (_currentSpriteIndex >= _sprites.Length)
+        {
+            if (_loop)
+            {
+                _currentSpriteIndex = 0;
+            }
+            else {
+                _isPlaying = false;
+                _onComplete?.Invoke();
+                return;
+            }
+        }
+
+        _spriteRenderer.sprite = _sprites[_currentSpriteIndex];
+        _currentSpriteIndex++;
+        _nextFrameTime += _secondsPerFrame;
+    }
+}
