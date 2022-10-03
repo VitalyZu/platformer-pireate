06f6ad9f2abf6394db097ab77abdc2871edd8c50
VitalyZu <vitaly.z.vz@gmail.com>
2022-10-04 00:13:37 +0300

    add-coins-prefab-animations

---
+++ Assets/Scripts/Components/SetCoinsComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCoinsComponent : MonoBehaviour
+{
+    public void SetHeroCoins(int coins)
+    {
+        GameObject.Find("Player").gameObject.GetComponent<Hero>().setCoins(coins);
+    }
+}
