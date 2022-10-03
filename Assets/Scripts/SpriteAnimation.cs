faf331a99a823dc7691be9e28ef36f9512e5542e
VitalyZu <vitaly.z.vz@gmail.com>
2022-10-04 02:16:29 +0300

    add-spriteanimation-clips

--- Assets/Scripts/SpriteAnimation.cs
+++ Assets/Scripts/SpriteAnimation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,15 +7,17 @@ using UnityEngine.Events;
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteAnimation : MonoBehaviour
 {
+    [SerializeField] private string _startName;
     [SerializeField] private int _frameRate;
     [SerializeField] private bool _loop;
-    [SerializeField] private Sprite[] _sprites;
     [SerializeField] private UnityEvent _onComplete;
+    [SerializeField] private AnimationClip[] _animationsClip;
 
     private SpriteRenderer _spriteRenderer;
     private float _secondsPerFrame;
     private int _currentSpriteIndex;
     private float _nextFrameTime;
+    private int? _clipIndex;
 
     private bool _isPlaying = true;
 
@@ -23,27 +26,69 @@ public class SpriteAnimation : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _secondsPerFrame = 1f / _frameRate;
         _nextFrameTime = Time.time + _secondsPerFrame;
+        
+        for (int i = 0; i < _animationsClip.Length; i++)
+        {
+            if (_animationsClip[i].name == _startName)
+            {
+                _clipIndex = i;
+            }
+        }
+        if (_clipIndex == null) {
+            if (_animationsClip.Length > 0) _clipIndex = 0;
+            else _isPlaying = false;
+        }
+        
     }
 
     private void Update()
     {
-        if (_isPlaying && _nextFrameTime > Time.time) return;
-
-        if (_currentSpriteIndex >= _sprites.Length)
+        if (_isPlaying)
         {
-            if (_loop)
+            SetClip((int)_clipIndex);
+        }
+        
+    }
+
+    private void SetClip(int index)
+    {
+        if (_nextFrameTime > Time.time) return;
+        if (_currentSpriteIndex >= _animationsClip[index].sprites.Length)
+        {
+            if (_animationsClip[index].allowNext)
+            {
+                if (index >= _animationsClip.Length - 1) {
+                    _clipIndex = 0;
+                }
+                else {
+                    _clipIndex++;
+                }
+
+                _currentSpriteIndex = 0;
+                return;
+            }
+            if (_animationsClip[index].loop)
             {
                 _currentSpriteIndex = 0;
             }
-            else {
+            else
+            {
                 _isPlaying = false;
                 _onComplete?.Invoke();
                 return;
             }
         }
-
-        _spriteRenderer.sprite = _sprites[_currentSpriteIndex];
+        _spriteRenderer.sprite = _animationsClip[index].sprites[_currentSpriteIndex];
         _currentSpriteIndex++;
         _nextFrameTime += _secondsPerFrame;
+    }
+
+    [Serializable]
+    public class AnimationClip
+    {
+        public string name;
+        public bool loop;
+        public Sprite[] sprites;
+        public bool allowNext;
     }
 }
