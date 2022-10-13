65e5ef6379b1ca0f7e65946f7ec1040d75d4f2b4
VitalyZu <vitaly.z.vz@gmail.com>
2022-10-13 23:46:21 +0300

    add-teleport-component

---
+++ Assets/Scripts/Components/TeleportComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportComponent : MonoBehaviour
+{
+    [SerializeField] Transform _moveTo;
+
+    public void Teleport(GameObject target)
+    {
+        target.transform.position = _moveTo.position;
+    }
+}
