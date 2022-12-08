6eb120370c0182249d4d2c05baeb2c32ffbde8b2
VitalyZu <vitaly.z.vz@gmail.com>
2022-12-09 01:44:51 +0300

    add-exitlevelcomponent

---
+++ Assets/Scripts/Components/ExitLevelComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitLevelComponent : MonoBehaviour
+{
+    [SerializeField] private string _sceneName;
+    public void Exit()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
