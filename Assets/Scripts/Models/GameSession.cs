2b89f1b052de4d355dae36ea4e3879ca101e4baa
VitalyZu <vitaly.z.vz@gmail.com>
2022-12-12 23:21:34 +0300

    add-GameSession-obj

---
+++ Assets/Scripts/Models/GameSession.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] private PlayerData _playerData;
+
+    public PlayerData Data { get; private set; }
+
+    private void Awake()
+    {
+        if (IsSessionExist())
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private bool IsSessionExist()
+    {
+        GameSession[] sessions = FindObjectsOfType<GameSession>();
+        foreach (GameSession session in sessions)
+        {
+            if (session != this)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Models/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class PlayerData 
+{ 
+    public int Coins;
+    public int HP;
+    public bool IsArmed;
+}
+    
+
