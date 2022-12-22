43d8db56d9c291e8b9cfc685d23284674ed87319
VitalyZu <vitaly.z.vz@gmail.com>
2022-12-23 00:21:53 +0300

    MobAI - p2

--- Assets/Scripts/Creatures/MobAI.cs
+++ Assets/Scripts/Creatures/MobAI.cs
@@ -9,17 +9,24 @@ public class MobAI : MonoBehaviour
     [SerializeField] LayerCheck _canAttack;
     [SerializeField] float _agroTime = .5f;
     [SerializeField] float _attackCooldown = 1f;
+    [SerializeField] float _missCooldown = 1f;
 
     private Coroutine _current;
     private GameObject _target;
 
     private SpawnListComponent _particles;
     private Creature _creature;
+    private Animator _animator;
+
+    private bool _isDead;
+
+    private static readonly int dieKey = Animator.StringToHash("die");
 
     private void Awake()
     {
         _particles = GetComponent<SpawnListComponent>();
         _creature = GetComponent<Creature>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Start()
@@ -29,6 +36,8 @@ public class MobAI : MonoBehaviour
 
     private void StartState(IEnumerator coroutine)
     {
+        _creature.SetDirection(Vector2.zero);
+
         if (_current != null) StopCoroutine(_current);
         
         _current = StartCoroutine(coroutine);
@@ -36,6 +45,8 @@ public class MobAI : MonoBehaviour
 
     public void OnEnterVision(GameObject go)
     {
+        if (_isDead) return;
+
         _target = go;
         StartState(AgroToHero());
     }
@@ -45,6 +56,13 @@ public class MobAI : MonoBehaviour
         var direction = _target.transform.position - transform.position;
         direction.y = 0;
         _creature.SetDirection(direction.normalized);
+    }
+
+    public void OnDie()
+    {
+        _animator.SetBool(dieKey, true);
+        _isDead = true;
+        if (_current != null) StopCoroutine(_current);
     }
 
     private IEnumerator Patrolling()
@@ -75,6 +93,10 @@ public class MobAI : MonoBehaviour
             
             yield return null;
         }
+        //_creature.SetDirection(Vector2.zero);
+        _particles.Spawn("Miss");
+
+        yield return new WaitForSeconds(_missCooldown);
     }
 
     private IEnumerator Attack()
