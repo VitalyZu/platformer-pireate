04332eb8221f305a756bcd9c9ccf2d95171d7bf5
VitalyZu <vitaly.z.vz@gmail.com>
2023-02-02 21:56:08 +0300

    add-projectile-script

---
+++ Assets/Scripts/Creatures/Weapons/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rb;
+    private int _direction;
+
+    private void Start()
+    {
+        _direction = transform.lossyScale.x > 0 ? 1 : -1;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var position = _rb.position;
+        position.x += _speed * _direction;
+        _rb.MovePosition(position);
+    }
+}
