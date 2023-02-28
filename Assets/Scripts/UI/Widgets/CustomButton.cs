027e2be5b452f3f0c17a2760038edee1baa4f18f
VitalyZu <vitaly.z.vz@gmail.com>
2023-02-28 23:28:44 +0300

    main-menu-ui-p1

---
+++ Assets/Scripts/UI/Widgets/CustomButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomButton : Button
+{
+    [SerializeField] GameObject _normal;
+    [SerializeField] GameObject _selected;
+    protected override void DoStateTransition(SelectionState state, bool instant)
+    {
+        base.DoStateTransition(state, instant);
+        _normal.SetActive(state != SelectionState.Pressed);
+        _selected.SetActive(state == SelectionState.Pressed);
+    }
+}
---
+++ Assets/Scripts/UI/Widgets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+
+[CustomEditor(typeof(CustomButton), true)]
+[CanEditMultipleObjects]
+public class CustomButtonEditor : ButtonEditor
+{   
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_normal"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_selected"));
+        serializedObject.ApplyModifiedProperties();
+        
+        base.OnInspectorGUI();
+    }
+}
