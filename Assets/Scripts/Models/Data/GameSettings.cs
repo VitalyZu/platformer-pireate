31e677f55aa8d6a716803765ee935680d6459506
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-01 16:29:33 +0300

    hud-data-model

---
+++ Assets/Scripts/Models/Data/GameSettings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/GameSettings", fileName = "GameSettings")]
+public class GameSettings : ScriptableObject
+{
+    [SerializeField] private FloatPersistentProperty Music;
+    [SerializeField] private FloatPersistentProperty Sfx;
+
+    //Singleton
+    private static GameSettings _instance;
+    public static GameSettings I => _instance == null ? LoadGameSettings() : _instance;
+    private static GameSettings LoadGameSettings()
+    {
+        return _instance = Resources.Load<GameSettings>("GameSettings");
+    }
+
+    private void OnEnable() 
+    {
+        //Обращаться нужно в OnEnable, т к в констукторе не даст обратиться к PlayerPrefs
+        Music = new FloatPersistentProperty(1, SoundSetting.Music.ToString());
+        Sfx = new FloatPersistentProperty(1, SoundSetting.SFX.ToString());
+
+    }
+
+    private void OnValidate()
+    {
+        Music.Validate();
+        Sfx.Validate();
+    }
+
+}
+
+public enum SoundSetting { 
+    Music,
+    SFX
+}
---
+++ Assets/Scripts/Models/Data/Properties/PersistentProperty.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PersistentProperty<TPropertyType>
+{
+    [SerializeField] private TPropertyType _value;  //Для инспектроа
+    private TPropertyType _stored;  //Значение записаное на диске
+
+    private TPropertyType _defaultValue;
+
+    public delegate void OnPropertyChanged(TPropertyType newValue, TPropertyType oldValue);
+    public event OnPropertyChanged onChanged;
+
+    public PersistentProperty(TPropertyType defualtValue)
+    {
+        _defaultValue = defualtValue;
+    }
+
+    public TPropertyType Value 
+        {
+            get => _stored;
+            set  {
+                var isEqual = _stored.Equals(value);
+                if (isEqual) return;
+
+                var oldValue = _value;
+                Write(value);
+                _stored = _value = value;
+                onChanged?.Invoke(value, oldValue);
+            }
+        }
+    protected void Init()
+    {
+        _stored = _value = Read(_defaultValue);
+    }
+    protected abstract void Write(TPropertyType value);
+    protected abstract TPropertyType Read(TPropertyType defaultValue);
+
+    public void Validate()
+    {
+        if (!_stored.Equals(_value)) Value = _value; 
+    }
+}
