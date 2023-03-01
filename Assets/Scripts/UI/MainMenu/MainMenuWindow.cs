e00c741795de18bad616219ca6a6c1036313ded6
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-01 11:44:26 +0300

    main-menu-animate-ui

---
+++ Assets/Scripts/UI/MainMenu/MainMenuWindow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class MainMenuWindow : AnimatedWindow
+{
+    private Action _closeAction;
+
+    public void OnShowSettings()
+    {
+        var window = Resources.Load("UI/SettingsWindow");
+        var canvas = FindObjectOfType<Canvas>();
+        Instantiate(window, canvas.transform);
+    }
+
+    public void OnStartGame()
+    {
+        _closeAction = () =>
+        {
+            SceneManager.LoadScene(0);
+        };
+        Close();
+    }
+
+    public void OnExit()
+    {
+        _closeAction = () =>
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        };
+        Close();
+    }
+
+    public override void OnCloseAnimationComplete()
+    {
+        base.OnCloseAnimationComplete();
+
+        _closeAction?.Invoke();
+    }
+}
