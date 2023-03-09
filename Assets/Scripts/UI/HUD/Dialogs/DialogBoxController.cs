f660c988eff494de013d720de7cbbee7473eda96
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-09 21:25:01 +0300

    dialog-box-controller

--- Assets/Scripts/UI/HUD/Dialogs/DialogBoxController.cs
+++ Assets/Scripts/UI/HUD/Dialogs/DialogBoxController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,13 +14,14 @@ public class DialogBoxController : MonoBehaviour
     [SerializeField] float _textSpeed = 0.09f;
 
     [Header("Sounds")]
-    [SerializeField] AudioClip _typing;
-    [SerializeField] AudioClip _open;
-    [SerializeField] AudioClip close;
+    [SerializeField] AudioClip _typingClip;
+    [SerializeField] AudioClip _openClip;
+    [SerializeField] AudioClip _closeClip;
 
     private DialogData _data;
     private int _currentSentence;
     private AudioSource _audioSource;
+    private Coroutine _typingRoutine;
 
     private readonly int _isOpenKey = Animator.StringToHash("isOpen");
 
@@ -34,8 +36,70 @@ public class DialogBoxController : MonoBehaviour
         _text.text = string.Empty;
 
         _container.SetActive(true);
-        _audioSource.PlayOneShot(_open);
+        _audioSource.PlayOneShot(_openClip);
         _animator.SetBool(_isOpenKey, true);
+    }
+
+    private IEnumerator TypeDialogTetx()
+    {
+        _text.text = string.Empty;
+        var sentence = _data.Sentences[_currentSentence];
+        foreach (var letter in sentence)
+        {
+            _text.text += letter;
+            _audioSource.PlayOneShot(_typingClip);
+            yield return new WaitForSeconds(_textSpeed);
+        }
+        _typingRoutine = null;
+    }
+
+    public void OnSkip()
+    {
+        if (_typingRoutine == null) return;
+
+        StopTypeAnimation();
+
+    }
+
+    public void OnContinue()
+    {
+        StopTypeAnimation();
+        _currentSentence++;
+
+        var isDialogComplete = _currentSentence >= _data.Sentences.Length;
+        
+        if (isDialogComplete)
+        {
+            HideDialogBox();
+        }
+        else
+        {
+            OnStartDialogAnimation();
+        }
+    }
+
+    private void HideDialogBox()
+    {
+        _animator.SetBool(_isOpenKey, false);
+        _audioSource.PlayOneShot(_closeClip);
+    }
+
+    private void StopTypeAnimation()
+    {
+        if (_typingRoutine != null) StopCoroutine(_typingRoutine);
+        _typingRoutine = null;
+        _text.text = _data.Sentences[_currentSentence];
+
+    }
+
+    private void OnStartDialogAnimation()
+    {
+        _typingRoutine = StartCoroutine(TypeDialogTetx());
+    }
+
+    private void OnClsoeDialogAnimationComplete()
+    {
+
     }
 
     [SerializeField] private DialogData _dataTest;
