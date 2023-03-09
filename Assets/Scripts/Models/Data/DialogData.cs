c7c5997ecf8d9f46d8a644e4f6de00b28905b3e4
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-09 17:32:02 +0300

    dialog-system-controller

---
+++ Assets/Scripts/Models/Data/DialogData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class DialogData
+{
+    [SerializeField] string[] _sentences;
+    public string[] Sentences => _sentences;
+}
---
+++ Assets/Scripts/UI/HUD/Dialogs/DialogBoxController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogBoxController : MonoBehaviour
+{
+    [SerializeField] Text _text;
+    [SerializeField] GameObject _container;
+    [SerializeField] Animator _animator;
+    
+    [Space]
+    [SerializeField] float _textSpeed = 0.09f;
+
+    [Header("Sounds")]
+    [SerializeField] AudioClip _typing;
+    [SerializeField] AudioClip _open;
+    [SerializeField] AudioClip close;
+
+    private DialogData _data;
+    private int _currentSentence;
+    private AudioSource _audioSource;
+
+    private readonly int _isOpenKey = Animator.StringToHash("isOpen");
+
+    private void Start()
+    {
+        _audioSource = AudioUtils.FindSfxSource();
+    }
+    public void ShowDialog(DialogData data)
+    {
+        _data = data;
+        _currentSentence = 0;
+        _text.text = string.Empty;
+
+        _container.SetActive(true);
+        _audioSource.PlayOneShot(_open);
+        _animator.SetBool(_isOpenKey, true);
+    }
+
+    [SerializeField] private DialogData _dataTest;
+    public void Test()
+    {
+        ShowDialog(_dataTest);
+    }
+}
