4f839c1d17b3702d6e9f8ea85e451f1d6fe89750
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-15 18:37:26 +0300

    parallax-&-infinite-clouds

---
+++ Assets/Scripts/Components/Effects/InfiniteBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteBackground : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Transform _container;
+
+    private Bounds _containerBounds;
+    private Bounds _allBounds;
+
+    private Vector3 _boundsToTransformDelta;
+    private Vector3 _containerDelta;
+
+    private void Start()
+    {
+        var sprites = _container.GetComponentsInChildren<SpriteRenderer>();
+        _containerBounds = sprites[0].bounds;
+        foreach (var sprite in sprites)
+        {
+            _containerBounds.Encapsulate(sprite.bounds);
+        }
+
+        _allBounds = _containerBounds;
+
+        _boundsToTransformDelta = transform.position - _allBounds.center;
+        _containerDelta = _container.position - _allBounds.center;
+    }
+
+    private void OnDrawGizmos()
+    {
+        GizmoUtils.DrawBounds(_allBounds, Color.magenta);
+    }
+}
