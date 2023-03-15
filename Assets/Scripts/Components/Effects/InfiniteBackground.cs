6ab4d9ca4ef289e39b4a2aaf4a3da74512168563
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-16 00:01:29 +0300

    inifinite-background-update

--- Assets/Scripts/Components/Effects/InfiniteBackground.cs
+++ Assets/Scripts/Components/Effects/InfiniteBackground.cs
@@ -12,6 +12,7 @@ public class InfiniteBackground : MonoBehaviour
 
     private Vector3 _boundsToTransformDelta;
     private Vector3 _containerDelta;
+    private Vector3 _screenSize;
 
     private void Start()
     {
@@ -26,6 +27,46 @@ public class InfiniteBackground : MonoBehaviour
 
         _boundsToTransformDelta = transform.position - _allBounds.center;
         _containerDelta = _container.position - _allBounds.center;
+    }
+
+    private void LateUpdate()
+    {
+        var min = _camera.ViewportToWorldPoint(Vector3.zero);
+        var max = _camera.ViewportToWorldPoint(Vector3.one);
+        _screenSize = new Vector3(max.x - min.x, max.y - min.y);
+
+        _allBounds.center = transform.position - _boundsToTransformDelta;
+        var camPosition = _camera.transform.position.x;
+
+        var screenLeft = new Vector3(camPosition - _screenSize.x / 2, _containerBounds.center.y);
+        var screenRight = new Vector3(camPosition + _screenSize.x / 2, _containerBounds.center.y);
+
+        if (!_allBounds.Contains(screenLeft))
+        {
+            InstantiateContainer(_allBounds.min.x - _containerBounds.extents.x);
+        }
+        if (!_allBounds.Contains(screenRight))
+        {
+            InstantiateContainer(_allBounds.max.x + _containerBounds.extents.x);
+        }
+    }
+
+    private void InstantiateContainer(float boundCenterX)
+    {
+        var newBounds = new Bounds(new Vector3(boundCenterX, _containerBounds.center.y), _containerBounds.size);
+        _allBounds.Encapsulate(newBounds);
+
+        _boundsToTransformDelta = transform.position - _allBounds.center;
+
+        var newContainerXPos = boundCenterX + _containerDelta.x;
+        var newPosition = new Vector3(newContainerXPos, _container.transform.position.y);
+
+        Instantiate(_container, newPosition, Quaternion.identity, transform);
+    }
+
+    private void InstantiateContainer(float x, float y)
+    {
+        throw new System.NotImplementedException();
     }
 
     private void OnDrawGizmos()
