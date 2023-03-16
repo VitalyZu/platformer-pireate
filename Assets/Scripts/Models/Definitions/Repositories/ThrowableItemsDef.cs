2af27ddd5633e68653da514a0dbc5cc3483607d4
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-16 23:45:12 +0300

    add-repositories
    Inventiry + throwable

---
+++ Assets/Scripts/Models/Definitions/Repositories/ThrowableItemsDef.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+[CreateAssetMenu(menuName = "Defs/ThrowableItemsDef", fileName = "ThrowableItemsDef")]
+public class ThrowableItemsDef : DefRepository<ThrowableDef>
+{
+}
+
+[Serializable]
+public struct ThrowableDef : IHaveId
+{
+    [InventoryId] [SerializeField] private string _id;
+    [SerializeField] private GameObject _projectile;
+
+    public string Id => _id;
+    public GameObject Projectile => _projectile;
+}
