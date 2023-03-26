f821c4e680c902ebd44db30d9fc01ef64754175c
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-27 00:17:31 +0300

    boss-ai-p1

---
+++ Assets/Scripts/Components/GOBased/CircularProjectileSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularProjectileSpawner : MonoBehaviour
+{
+    [SerializeField] DirectionalProjectile _projectile;
+    [SerializeField] int _burstCount;
+    [SerializeField] float _delay;
+
+    [ContextMenu("Launch")]
+    public void LaunchProjectiles()
+    {
+        StartCoroutine(SpawnProjectiles());
+    }
+  
+    private IEnumerator SpawnProjectiles()
+    {
+        var sectorStep = 2 * Mathf.PI / _burstCount;
+
+        for (int i = 0; i < _burstCount; i++)
+        {
+            var angle = sectorStep * i;
+            var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+            var instance = SpawnUtils.Spawn(_projectile.gameObject, transform.position);
+            var projectile = instance.GetComponent<DirectionalProjectile>();
+            projectile.Launch(direction);
+            
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+}
---
+++ Assets/Scripts/Creatures/Weapons/DirectionalProjectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalProjectile : BaseProjetile
+{
+    public void Launch(Vector2 direction)
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+        Rigidbody.AddForce(direction * _speed, ForceMode2D.Impulse);
+        //Rigidbody.AddForce(Vector2.up, ForceMode2D.Impulse);
+    }
+}
