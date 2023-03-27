d39bf92830e7bfadefe37f9092ccbfe97b699674
VitalyZu <vitaly.z.vz@gmail.com>
2023-03-27 16:05:27 +0300

    boss-ai-diffic

--- Assets/Scripts/Components/GOBased/CircularProjectileSpawner.cs
+++ Assets/Scripts/Components/GOBased/CircularProjectileSpawner.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class CircularProjectileSpawner : MonoBehaviour
 {
-    [SerializeField] DirectionalProjectile _projectile;
-    [SerializeField] int _burstCount;
-    [SerializeField] float _delay;
+    [SerializeField] private CircularProjectileSettings[] _settings;
+
+    public int Stage { get; set; }
 
     [ContextMenu("Launch")]
     public void LaunchProjectiles()
@@ -17,18 +17,31 @@ public class CircularProjectileSpawner : MonoBehaviour
   
     private IEnumerator SpawnProjectiles()
     {
-        var sectorStep = 2 * Mathf.PI / _burstCount;
+        var settings = _settings[Stage];
+        var sectorStep = 2 * Mathf.PI / settings.BurstCount;
 
-        for (int i = 0; i < _burstCount; i++)
+        for (int i = 0; i < settings.BurstCount; i++)
         {
             var angle = sectorStep * i;
             var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
 
-            var instance = SpawnUtils.Spawn(_projectile.gameObject, transform.position);
+            var instance = SpawnUtils.Spawn(settings.Projectile.gameObject, transform.position);
             var projectile = instance.GetComponent<DirectionalProjectile>();
             projectile.Launch(direction);
             
-            yield return new WaitForSeconds(_delay);
+            yield return new WaitForSeconds(settings.Delay);
         }
     }
+}
+
+[Serializable]
+public struct CircularProjectileSettings
+{
+    [SerializeField] private DirectionalProjectile _projectile;
+    [SerializeField] private int _burstCount;
+    [SerializeField] private float _delay;
+
+    public DirectionalProjectile Projectile => _projectile;
+    public int BurstCount => _burstCount;
+    public float Delay => _delay;
 }
---
+++ Assets/Scripts/Creatures/Boss/BossNextState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossNextState : StateMachineBehaviour
+{
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var spawner = animator.GetComponent<CircularProjectileSpawner>();
+        spawner.Stage++;
+
+        var changeColor = animator.GetComponent<ChangeLightComponent>();
+        changeColor.SetColor();
+    }
+}
